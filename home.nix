e977b0ca265a4eb5841c2fcad5da0b821f5c4ebb
arpadav <arpadav@gmail.com>
2026-03-24 16:52:51 -0400

    whoops

--- home.nix
+++ home.nix
@@ -92,7 +92,7 @@
       source "$HOME/.config/broot/launcher/bash/br"
 
       # export pkgconfig path
-      export PKG_CONFIG_PATH=/usr/lib/pkgconfig
+      export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
     '';
 
     # --------------------------------------------------
